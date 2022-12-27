c67a60f1663f2f3f582de771630d1d2c9ccfd12e
Charlie Laughton <charles.laughton@nottingham.ac.uk>
2022-12-27 20:25:00 +0000

    formatting corrections

--- WElib/base.py
+++ WElib/base.py
@@ -1,14 +1,17 @@
 import numpy as np
-from pathlib import Path
-import json
+
+# from pathlib import Path
+
+# import json
 import itertools
 
-class Walker(object):
-    '''
-    A WE simulation walker
-    '''
-    iterator = itertools.count()
 
+class Walker(object):
+    """
+    A WE simulation walker
+    """
+
+    iterator = itertools.count()
 
     def __init__(self, state, weight, state_id=None):
         self.state = state
@@ -23,7 +26,7 @@ class Walker(object):
         self._initial_state_id = self.state_id
         self._initial_pcs = None
         self.data = {}
-        
+
     def copy(self):
         new = Walker(self.state, self.weight, self.state_id)
         new.pcs = self.pcs
@@ -37,6 +40,7 @@ class Walker(object):
         self.state = self._initial_state
         self.state_id = self._initial_state_id
         self.pcs = self._initial_pcs
+        self.history = []
 
     def update(self, state):
         self.history.append(self.state_id)
@@ -47,7 +51,9 @@ class Walker(object):
 
     def merge(self, other_walker):
         w_tot = self.weight + other_walker.weight
-        i = np.random.choice([0,1], p=[self.weight / w_tot, other_walker.weight / w_tot])
+        i = np.random.choice(
+            [0, 1], p=[self.weight / w_tot, other_walker.weight / w_tot]
+        )
         if i == 0:
             new_walker = self.copy()
         else:
@@ -62,7 +68,11 @@ class Walker(object):
         return self, new_walker
 
     def __repr__(self):
-        return ('<WElib.Walker weight {}, progress coordinate {}, bin assignment {}>'.format(self.weight, self.pcs, self.bin_id))
+        return "<WElib.Walker weight {}, progress coordinate {}, \
+            bin assignment {}>".format(
+            self.weight, self.pcs, self.bin_id
+        )
+
 
 class FunctionStepper(object):
     # Move the walkers according to a supplied function
@@ -80,6 +90,7 @@ class FunctionStepper(object):
             w.update(state)
         self.recorder.record(walkers)
         return walkers
+
 
 class Recorder(object):
     def __init__(self):
@@ -104,10 +115,11 @@ class Recorder(object):
             all_states.append(w.state_id)
             all_states += w.history
         all_states = set(all_states)
-        
+
         for state_id in self.states:
-            if not state_id in all_states:
+            if state_id not in all_states:
                 del self.states[state_id]
+
 
 class FunctionProgressCoordinator(object):
     def __init__(self, pcfunc, *args):
@@ -127,17 +139,19 @@ class FunctionProgressCoordinator(object):
                 w._initial_pcs = w.pcs
         return walkers
 
+
 class StaticBinner(object):
-    '''
+    """
     A WE simulation bin classifier
-    
+
     Adds bin ID information to a list of walkers
 
     IDs are integers if there is just one dimension of binning,
     or tuples if more.
-    
+
     Initialised with a list of the bin edges.
-    '''
+    """
+
     def __init__(self, edges):
         self.edges = edges
         e = edges[0]
@@ -149,14 +163,13 @@ class StaticBinner(object):
         if n_dim == 1:
             self.edges = [self.edges]
         self.bin_weights = {}
-        
-    
+
     def run(self, walkers):
         if not isinstance(walkers, list):
             walkers = [walkers]
         pcs = np.atleast_2d([w.pcs for w in walkers])
         if None in pcs:
-            raise TypeError('Error: missing progress coordinates...')
+            raise TypeError("Error: missing progress coordinates...")
         bin_ids = []
         for dim in range(self.ndim):
             bin_ids.append(np.digitize(pcs[:, dim], self.edges[dim]))
@@ -166,7 +179,7 @@ class StaticBinner(object):
             bin_ids = bin_ids[0]
         for i, bin_id in enumerate(bin_ids):
             walkers[i].bin_id = bin_id
-            if not bin_id in self.bin_weights:
+            if bin_id not in self.bin_weights:
                 self.bin_weights[bin_id] = 0.0
             self.bin_weights[bin_id] += walkers[i].weight
         sorted_dict = {}
@@ -178,18 +191,21 @@ class StaticBinner(object):
     def reset(self):
         for k in self.bin_weights:
             self.bin_weights[k] = 0.0
+
+
 class Recycler(object):
-    '''
+    """
     A WE simulation recycler
-    
+
     Moves walkers that have reached the target pc back to the start
     Reports the recycled flux as well.
-    
+
     Initialised with the
     value of the target pc. If retrograde == False, then recycling happens if
     target_pc is exceeded, else reycling happens if
     the pc falls below target_pc.
-    '''
+    """
+
     def __init__(self, target_pcs, retrograde=False):
         self.target_pcs = target_pcs
         if isinstance(target_pcs, (list, tuple)):
@@ -205,7 +221,7 @@ class Recycler(object):
         self.recycled_walkers = []
         self.flux_history = []
         self.flux = None
-        
+
     def run(self, walkers):
         self.recycled_walkers = []
         self.flux = 0.0
@@ -213,46 +229,55 @@ class Recycler(object):
             walkers = [walkers]
         for i in range(len(walkers)):
             if walkers[i].pcs == []:
-                raise TypeError('Error - missing progress coordinate')
+                raise TypeError("Error - missing progress coordinate")
             recycle = True
             for idim in range(self.n_dim):
                 if self.target_pcs[idim] is not None:
                     if not self.retrograde[idim]:
-                        recycle = recycle and walkers[i].pcs[idim] > self.target_pcs[idim]
+                        recycle = (
+                            recycle and walkers[i].pcs[idim] > self.target_pcs[idim]
+                        )
                     else:
-                        recycle = recycle and walkers[i].pcs[idim] < self.target_pcs[idim]
+                        recycle = (
+                            recycle and walkers[i].pcs[idim] < self.target_pcs[idim]
+                        )
             if recycle:
-                replacement = Walker(walkers[i]._initial_state, walkers[i].weight,
-                                     walkers[i]._initial_state_id)
+                replacement = Walker(
+                    walkers[i]._initial_state,
+                    walkers[i].weight,
+                    walkers[i]._initial_state_id,
+                )
                 self.recycled_walkers.append(walkers[i])
                 walkers[i] = replacement
-                #walkers[i].restart()
                 weight = walkers[i].weight
-                self.flux += weight 
+                self.flux += weight
         self.flux_history.append(self.flux)
         return walkers
 
+
 class Bin(object):
-    '''
+    """
     A WE simulation bin object - only used internally
-    '''
+    """
+
     def __init__(self, index):
         self.index = index
         self.walkers = []
-        
+
     def add(self, walkers):
         if not isinstance(walkers, list):
             walkers = [walkers]
         self.walkers += walkers
-        
+
     def split_merge(self, target_size):
         if len(self.walkers) == target_size:
             ids = range(target_size)
         else:
             probs = np.array([w.weight for w in self.walkers])
             old_weight = probs.sum()
-            ids = np.random.choice(range(len(self.walkers)), 
-                    target_size, p=probs/old_weight)
+            ids = np.random.choice(
+                range(len(self.walkers)), target_size, p=probs / old_weight
+            )
         new_walkers = []
         for i in ids:
             new_walker = self.walkers[i].copy()
@@ -263,34 +288,35 @@ class Bin(object):
             for i in range(len(new_walkers)):
                 new_walkers[i].weight *= fac
         self.walkers = list(new_walkers)
-        
+
+
 class SplitMerger(object):
-    '''
+    """
     A WE simulation splitter and merger
-    
+
     Splits or merges the walkers in each bin to get to the required number of replicas
-    
+
     Initialised with the desired number of replicas in each bin.
-    '''
+    """
+
     def __init__(self, target_size):
         self.target_size = target_size
-    
+
     def run(self, walkers):
         if not isinstance(walkers, list):
             walkers = [walkers]
-            
+
         bins = {}
         for w in walkers:
-            if not w.bin_id in bins:
+            if w.bin_id not in bins:
                 bins[w.bin_id] = Bin(w.bin_id)
             bins[w.bin_id].add(w)
-        
+
         for bin in bins:
             bins[bin].split_merge(self.target_size)
-        
+
         new_walkers = []
         for bin in bins:
             new_walkers += bins[bin].walkers
-            
-        return new_walkers
 
+        return new_walkers
